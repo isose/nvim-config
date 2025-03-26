80a501bd5a372602b66b66a59f2cc9d85b646b9d
isose <theisose@gmail.com>
2025-03-26 07:15:53 -0700

    lazy load mini.nvim

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,19 +1,24 @@
 return {
   {
     "echasnovski/mini.nvim",
-    verion = false,
+    version = false,
+    event = { "InsertEnter" },
+    keys = {
+      {
+        "-",
+        function()
+          if not require("mini.files").close() then
+            require("mini.files").open(vim.api.nvim_buf_get_name(0))
+          end
+        end,
+        desc = "toggle open at current directory",
+      }
+    },
     config = function()
       require("mini.icons").setup()
       require("mini.pairs").setup()
       require("mini.surround").setup()
       require("mini.files").setup()
     end,
-
-    -- toggle open at current directory
-    vim.keymap.set("n", "-", function()
-      if not MiniFiles.close() then
-        MiniFiles.open(vim.api.nvim_buf_get_name(0))
-      end
-    end)
   },
 }
