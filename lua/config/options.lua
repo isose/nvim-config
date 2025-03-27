652ad407dc774427eec909a5e3a9a339dbd688c1
isose <theisose@gmail.com>
2025-03-27 05:27:21 -0700

    add keymap to insert semicolon

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -37,6 +37,8 @@ vim.keymap.set("n", "<Space>dd", "<cmd>lua vim.diagnostic.open_float()<CR>")
 vim.keymap.set("n", "<Space>dn", "<cmd>lua vim.diagnostic.goto_next()<CR>")
 vim.keymap.set("n", "<Space>dp", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
 
+vim.keymap.set('i', ';;', '<C-o>A;', { noremap = true }) -- insert ; at end of line
+
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
