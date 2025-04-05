be5492bbdee3fd4df4bbfb2a050c896fd87d9089
isose <theisose@gmail.com>
2025-04-04 18:49:21 -0700

    show documentation for autocomplete

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -14,6 +14,12 @@ return {
       },
       cmdline = { enabled = false },
       signature = { enabled = true },
+      completion = {
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 250,
+        },
+      },
       appearance = {
         nerd_font_variant = 'mono'
       },
