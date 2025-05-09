8c562496c9c9d8fc8b7f26bcfa16666b42582bf8
isose <theisose@gmail.com>
2025-05-08 19:19:57 -0700

    update mason config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,24 +1,12 @@
 return {
   {
     "mason-org/mason.nvim",
-    version = "1.11.0", -- pin version remove later
-    cmd = { "Mason" },
-    config = function()
-      require("mason").setup()
-    end,
+    opts = {}
   },
   {
     "mason-org/mason-lspconfig.nvim",
-    version = "1.32.0", -- pin version remove later
     event = { "BufReadPost", "BufNewFile" },
-    dependencies = { "mason.nvim" },
-    config = function()
-      require("mason-lspconfig").setup()
-      require("mason-lspconfig").setup_handlers({
-        function(server_name)
-          require("lspconfig")[server_name].setup({})
-        end,
-      })
-    end,
+    dependencies = { "neovim/nvim-lspconfig" },
+    opts = {}
   },
 }
